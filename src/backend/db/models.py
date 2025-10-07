a234c2b3e5d5ebcc83247d7e1a34a44abf48fcfb
GriffinBonomo <98484718+GriffinBonomo@users.noreply.github.com>
2025-10-07 19:29:41 -0400

    Create user data model

---
+++ src/backend/db/models.py
@@ -0,0 +1,18 @@
+from db import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    role = db.Columm(db.String(40))
+
+    @property
+    def data(self):
+        return{
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "role": self.role
+        }
