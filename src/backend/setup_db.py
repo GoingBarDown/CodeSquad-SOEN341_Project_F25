87a17cc28fc9145f145ced18ff3ca8a596b2675b
GriffinBonomo <98484718+GriffinBonomo@users.noreply.github.com>
2025-10-07 20:56:05 -0400

    Remove deprecated db_utils and update db initialization

--- src/backend/setup_db.py
+++ src/backend/setup_db.py
@@ -1,4 +1,12 @@
-from db.db_utils import init_db
+from flask import Flask
+from config import Config
+from db import db
 
-if __name__ == "__main__":
-    init_db()
+app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
+
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+    print("Database reset successfully.")
