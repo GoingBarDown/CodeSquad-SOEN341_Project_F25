094db9f8e97c9ff6c753f682ba96619d22a6954f
GriffinBonomo <98484718+GriffinBonomo@users.noreply.github.com>
2025-10-23 17:56:45 -0400

    Add mock tests for backend testing

---
+++ src/backend/db/tests/tests.py
@@ -0,0 +1,81 @@
+
+# Users Testing
+def test_user_add(user_data):
+    pass
+
+def test_user_get_all():
+    pass
+
+def test_user_get_one(id):
+    pass
+
+def test_user_edit(id, user_data):
+    pass
+
+def test_user_delete(id):
+    pass
+
+
+# Events Testing
+def test_event_add(event_data):
+    pass
+
+def test_event_get_all():
+    pass
+
+def test_event_get_one(id):
+    pass
+
+def test_event_edit(id, event_data):
+    pass
+
+def test_event_delete(id):
+    pass
+
+# Tickets Testing
+def test_ticket_add(ticket_data):
+    pass
+
+def test_ticket_get_all():
+    pass
+
+def test_ticket_get_one(id):
+    pass
+
+def test_ticket_edit(id, ticket_data):
+    pass
+
+def test_ticket_delete(id):
+    pass
+
+# Organization Testing
+def test_org_add(org_data):
+    pass
+
+def test_org_get_all():
+    pass
+
+def test_org_get_one(id):
+    pass
+
+def test_org_edit(id, org_data):
+    pass
+
+def test_org_delete(id):
+    pass
+
+# Organization Member Testing
+def test_org_member_add(org_member_data):
+    pass
+
+def test_org_member_get_all():
+    pass
+
+def test_org_member_get_one(id, org_id):
+    pass
+
+def test_org_member_edit(id, org_id, org_member_data):
+    pass
+
+def test_org_member_delete(id, org_id):
+    pass
