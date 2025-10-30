0695fb4d6d6d8eee6fe23cc37d50a47c3407dbd3
Noorjahan Kazi <noorkaazi@gmail.com>
2025-10-29 21:18:46 -0400

    Update index.js

--- src/frontend/eventExperience-frontend/index.js
+++ src/frontend/eventExperience-frontend/index.js
@@ -1,1 +1,7 @@
-
+const dot=document.getElementById('dot');
+const menu=document.getElementById('menu');
+dot.onclick=()=>{
+  const isOpen=menu.classList.toggle('open');
+  dot.innerHTML=isOpen?'&#8211;':'&#8801;';
+  //minus and menu symbols
+};
