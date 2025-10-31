da377b817d4e1eb64ff0009a2bc131007d45475d
Noorjahan Kazi <noorkaazi@gmail.com>
2025-10-31 00:51:34 -0400

    Update index.js
    slideshow functions

--- src/frontend/eventExperience-frontend/index.js
+++ src/frontend/eventExperience-frontend/index.js
@@ -2,6 +2,27 @@ const dot=document.getElementById('dot');
 const menu=document.getElementById('menu');
 dot.onclick=()=>{
   const isOpen=menu.classList.toggle('open');
-  dot.innerHTML=isOpen?'&#8211;':'&#8801;';
+  dot.innerHTML=isOpen?'&#8211;':'&#8801;';}
   //minus and menu symbols
-};
+
+const images = [
+  "views/image.png", //ew
+  "views/hackathon.png", //good
+  "views/hockey.png", //good
+  "views/jobfair.png", //switch
+  "views/october.png", //nicee
+  "views/openhouse.png" //nice
+];
+
+let index = 0;
+
+function changeBackground() {
+  document.getElementById("slideshow").style.backgroundImage = `url('${images[index]}')`;
+  index = (index + 1) % images.length;
+}
+
+//initial image
+changeBackground();
+
+//change every 30 seconds (5000 ms)
+setInterval(changeBackground, 10000);
